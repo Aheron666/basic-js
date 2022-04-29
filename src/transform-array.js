3fb3297f9f1eb82eb2afdfa7deb5870c16e50269
Suhov Oleg <aheronlcv@gmail.com>
2022-04-29 17:21:01 +0500

    task 6.3

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,42 +9,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * @example
  * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+ * transform( ) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
 function transform(/* arr */) {
-    let discardNext = '--discard-next';
-    let discardPrev = '--discard-prev';
-    let doubleNext = '--double-next';
-    let doublePrev = '--double-prev';
-    let newArr = arr;
-    if (Array.isArray(newArr)){
-      newArr.map((item, index) => {
-          if (item === discardNext) {
-              newArr.splice((newArr.indexOf(newArr[index])), 1)
-              newArr.splice((newArr.indexOf(newArr[index])), 1)
-          }
-          if (item === discardPrev) {
-              newArr.splice((newArr.indexOf(newArr[index])), 1);
-              newArr.splice((newArr.indexOf(newArr[index - 1])), 1)
-          }
-          if (item === doubleNext) {
-              newArr.splice((newArr.indexOf(newArr[index])), 1);
-              newArr[index] = newArr[index] * 2
-          }
-          if (item === doublePrev) {
-              newArr.splice((newArr.indexOf(newArr[index])), 1);
-              newArr[index - 1] = newArr[index - 1] * 2
-          }
-          return newArr;
-        })
-    }
-  
-    else newArr = "\'arr\' parameter must be an instance of the Array!"
-   
-    return newArr; 
-  
+  let discardNext = '--discard-next';
+  let discardPrev = '--discard-prev';
+  let doubleNext = '--double-next';
+  let doublePrev = '--double-prev';
+  let newArr = arr;
+  if (Array.isArray(newArr)){
+    newArr.map((item, index) => {
+        if (item === discardNext) {
+            if (index === newArr.length - 1){
+                newArr.splice((newArr.indexOf(newArr[index])), 1);
+            }
+            else {
+                newArr.splice((newArr.indexOf(newArr[index])), 1)
+                newArr.splice((newArr.indexOf(newArr[index])), 1)
+            }
+        }
+        if (item === discardPrev) {
+            if (index === 0){
+                newArr.splice((newArr.indexOf(newArr[index])), 1);
+            }
+            else {
+                newArr.splice((newArr.indexOf(newArr[index])), 1);
+                newArr.splice((newArr.indexOf(newArr[index - 1])), 1)
+            }
+        }
+        if (item === doubleNext) {
+            newArr[index] = newArr[index+1]
+            if (index === newArr.length - 1){
+                newArr.splice((newArr.indexOf(newArr[index])), 1);
+            }
+            // newArr.splice((newArr.indexOf(newArr[index])), 1, newArr[index+1]);
+        }
+        if (item === doublePrev) {
+            newArr[index] = newArr[index - 1]
+            if (index === 0){
+                newArr.splice((newArr.indexOf(newArr[index])), 1);
+            }
+            // newArr.splice((newArr.indexOf(newArr[index])), 1 , newArr[index - 1]);
+        }
+        return newArr;
+      })
+  }
+
+  else newArr = "\'arr\' parameter must be an instance of the Array!"
+ 
+  return newArr; 
 }
 
 module.exports = {
